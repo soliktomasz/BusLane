9469a931b1c9b1ea9ce70323daa987a5c94a3613
Tomasz Solik <tomek.solik@gmail.com>
2025-12-20 18:37:15 +0100

    Adds macOS dock icon support
    Adds code to set the application's dock icon on macOS.
    
    This enhances the user experience on macOS by providing a
    visual representation of the application in the dock.
    
    It silently fails if icon setting fails to avoid crashes.

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
@@ -17,6 +20,7 @@ public partial class App : Application
         Instance = this;
         AvaloniaXamlLoader.Load(this);
         ApplyTheme(Preferences.Theme);
+        SetMacOSDockIcon();
     }
 
     public override void OnFrameworkInitializationCompleted()
@@ -38,4 +42,57 @@ public partial class App : Application
             "System" or _ => ThemeVariant.Default
         };
     }
+    
+    private static void SetMacOSDockIcon()
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            return;
+
+        try
+        {
+            var iconPath = Path.Combine(AppContext.BaseDirectory, "icon.icns");
+            if (!File.Exists(iconPath))
+                return;
+
+            var nsImage = objc_getClass("NSImage");
+            var alloc = sel_registerName("alloc");
+            var initWithContentsOfFile = sel_registerName("initWithContentsOfFile:");
+            var setApplicationIconImage = sel_registerName("setApplicationIconImage:");
+            var sharedApplication = sel_registerName("sharedApplication");
+            
+            var nsApp = objc_getClass("NSApplication");
+            var sharedApp = objc_msgSend(nsApp, sharedApplication);
+            
+            var nsString = objc_getClass("NSString");
+            var stringWithUTF8String = sel_registerName("stringWithUTF8String:");
+            var pathString = objc_msgSend(nsString, stringWithUTF8String, iconPath);
+            
+            var imageAlloc = objc_msgSend(nsImage, alloc);
+            var image = objc_msgSend(imageAlloc, initWithContentsOfFile, pathString);
+            
+            if (image != IntPtr.Zero)
+            {
+                objc_msgSend(sharedApp, setApplicationIconImage, image);
+            }
+        }
+        catch
+        {
+            // Silently fail if we can't set the icon
+        }
+    }
+    
+    [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_getClass")]
+    private static extern IntPtr objc_getClass(string className);
+    
+    [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "sel_registerName")]
+    private static extern IntPtr sel_registerName(string selectorName);
+    
+    [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
+    private static extern IntPtr objc_msgSend(IntPtr receiver, IntPtr selector);
+    
+    [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
+    private static extern IntPtr objc_msgSend(IntPtr receiver, IntPtr selector, IntPtr arg1);
+    
+    [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
+    private static extern IntPtr objc_msgSend(IntPtr receiver, IntPtr selector, string arg1);
 }
