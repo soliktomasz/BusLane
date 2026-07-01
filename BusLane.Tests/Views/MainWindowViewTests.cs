b53a6eb0a999f09b55827b895ecb66c369f6a275
Tomasz Solik <tomasz.solik@komplett.com>
2026-07-01 12:58:07 +0200

    fix: Correct display of status and error messages

--- BusLane.Tests/Views/MainWindowViewTests.cs
+++ BusLane.Tests/Views/MainWindowViewTests.cs
@@ -15,6 +15,19 @@ public class MainWindowViewTests
         xaml.Should().Contain("Foreground=\"{DynamicResource SubtleForeground}\"");
     }
 
+    [Fact]
+    public void MainWindow_StatusArea_ShowsFullShellStatusWithoutPopupTrigger()
+    {
+        // Arrange
+        var xaml = File.ReadAllText(GetMainWindowPath());
+
+        // Assert
+        xaml.Should().Contain("Text=\"{Binding ShellStatusMessage}\"");
+        xaml.Should().Contain("TextWrapping=\"Wrap\"");
+        xaml.Should().NotContain("ToggleStatusPopupCommand");
+        xaml.Should().NotContain("Click to see full message");
+    }
+
     private static string GetMainWindowPath()
     {
         return Path.GetFullPath(Path.Combine(
